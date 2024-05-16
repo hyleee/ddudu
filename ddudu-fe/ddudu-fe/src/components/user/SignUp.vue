a7d69d637ca05b9463f0f536c23b86e74fdcf32e
unknown <commonhm@gmail.com>
2024-05-16 17:58:09 +0900

    feat : regist fe-be 연동

--- ddudu-fe/ddudu-fe/src/components/user/SignUp.vue
+++ ddudu-fe/ddudu-fe/src/components/user/SignUp.vue
@@ -7,35 +7,35 @@
     </div>
     <div>
       <label for="id">ID : </label>
-      <input type="text" id="id" v-model="store.user.id">
+      <input type="text" id="id" v-model="store.user.userId">
     </div>
     <div>
       <label for="password">PW : </label>
-      <input type="text" id="password" v-model="store.user.password">
+      <input type="text" id="password" v-model="store.user.userPassword">
     </div>
     <div>
       <label for="name">이름 : </label>
-      <input type="text" id="name" v-model="store.user.name">
+      <input type="text" id="name" v-model="store.user.userName">
     </div>
     <div>
       <label for="email">이메일 : </label>
-      <input type="email" id="email" v-model="store.user.email">
+      <input type="email" id="email" v-model="store.user.userEmail">
     </div>
     <div>
       <label for="age">나이 : </label>
-      <input type="number" id="age" v-model="store.user.age">
+      <input type="number" id="age" v-model="store.user.userAge">
     </div>
     <div>
       <label for="height">키 : </label>
-      <input type="number" id="height" v-model="store.user.height">
+      <input type="number" id="height" v-model="store.user.userHeight">
     </div>
     <div>
       <label for="weight">체중 : </label>
-      <input type="number" id="weight" v-model="store.user.weight">
+      <input type="number" id="weight" v-model="store.user.userWeight">
     </div>
     <div>
       <label for="area">지역 : </label>
-      <input type="text" id="area" v-model="store.user.area">
+      <input type="text" id="area" v-model="store.user.userArea">
     </div>
     <div>
       <button @click="userCreate">등록</button>
